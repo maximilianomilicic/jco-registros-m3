00ed18d926348d57562dc00d11cc9ce77a9e28d8
maxi <maximiliano.milicic@gmail.com>
2023-04-06 10:37:21 -0300

    Idioma
    se agregó la zona horia y la hora de argentina a django

--- m3/m3/views.py
+++ m3/m3/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse 
+from registrosdem3.registros import models
 
 
 def saludo(self):
+    datos = Nombre = Nombre.Nombre 
     return HttpResponse('holaaaaaaaaaaaaa')
 
 
