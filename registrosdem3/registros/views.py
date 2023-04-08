80274c56a76ccd8e2cb95cbdf46ee5a185d35f05
maxi <maximiliano.milicic@gmail.com>
2023-04-08 19:09:54 -0300

    agregamos Bootstrap
    .

--- registrosdem3/registros/views.py
+++ registrosdem3/registros/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Template, Context
 from django.template import loader
+from registros.models import Nombre
 
 # Create your views here.
 
 def Home(request):
+    listado = Nombre.objects.all()
     template = loader.get_template('home.html')
     documento = template.render()
     return HttpResponse(documento)
